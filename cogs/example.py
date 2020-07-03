452d3d9e493ed9e67fc883dc00da59cf1f671ae9
abajajsingh <aikasbajaj@protonmail.com>
2020-07-03 04:45:12 -0400

    establishes formatting for cogs!

---
+++ cogs/example.py
@@ -0,0 +1,47 @@
+# An example of how a Cog should be properly instantiated
+
+import discord
+from discord.ext import commands
+
+class Example(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is online.")
+    
+    # Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Pong!")
+    
+    @commands.command(aliases=['8ball', 'test'])
+    async def _8ball(ctx, *, question):
+        responses = ['It is a fact.',
+                    'Winner winner, chicken dinner.',
+                    'Could it have been any different?',
+                    'Yes, definitely.',
+                    'Without a doubt.',
+                    'As I see it, yes.',
+                    'Outlook is favorable.',
+                    'Yes.',
+                    "It's only a matter of time.",
+                    'Signs point to yes.',
+                    'Ask again later.',
+                    'Have you been drinking enough water? Ask again.',
+                    'Better not tell you now.',
+                    'Concentrate and ask again.',
+                    'Nah fam.',
+                    'Not likely.',
+                    'Very doubtful.',
+                    'Yes, but only if you inlucde a friend.']
+        await ctx.send(f'Q: {question}\nA: {random.choice(responses)}')
+
+    @commands.command()
+    async def joined(ctx, *, member: discord.Member):
+        await ctx.send('{discord.member} joined on {0.joined_at}'.format(member))
+
+def setup(client):
+    client.add_cog(Example(client))

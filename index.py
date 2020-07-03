12c893090ce5c768ce675f4165bd442b68109463
abajajsingh <aikasbajaj@protonmail.com>
2020-07-03 18:44:47 -0400

    creates moderation cog

--- index.py
+++ index.py
@@ -14,6 +14,14 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
 
+def embedsText(title, description):
+    if title == '':
+        return discord.Embed(description=description, color=0x228B22)
+    elif description == '':
+        return discord.Embed(title=title, color=0x228B22)
+    else:
+        return discord.Embed(description=description, title=title, color=0x228B22)
+
 # loads cogs into bot
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):

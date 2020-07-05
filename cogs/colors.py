3f509df32767028710d921d280d12be9e81cb407
abajajsingh <aikasbajaj@protonmail.com>
2020-07-05 14:49:47 -0400

    adds !scheme command for a monochrome based color scheme

--- cogs/colors.py
+++ cogs/colors.py
@@ -16,15 +16,11 @@ class Colors(commands.Cog):
     
     # Takes the dominant color of the image in a link and sends the color
     @commands.command()
-    async def color(self, ctx, arg1):
-        # takes photo url and saves it
+    async def color(self, ctx, arg1, numColors=1:int):
+        message = await ctx.send(f"{ctx.message.author.mention} detecting color... might take a few seconds")
         opener = urllib.request.URLopener()
-        opener.addheader('User-Agent', 'whatever')
-        filename, headers = opener.retrieve(arg1, 'images/imageRecieved.jpg')
-
-        # gets dominant color from saved photo
-        color_thief = ColorThief('images/imageRecieved.jpg')
-        dominant_rgb = color_thief.get_color(quality=1)
+        self.convertsIMGRecieved(arg1, opener)
+        
         
         # gets photo of that color in svg
         jsonurl = urlopen("http://www.thecolorapi.com/id?rgb=rgb({0},{1},{2})".format(dominant_rgb[0],dominant_rgb[1],dominant_rgb[2]))
@@ -34,25 +30,20 @@ class Colors(commands.Cog):
         filename, headers = opener.retrieve(text["image"]["bare"], 'images/tempSVG.svg')
 
         # converts photo into usable format and sends
-        drawing = svg2rlg("images/tempSVG.svg")
-        renderPM.drawToFile(drawing, "images/imageSend.png", fmt="PNG")
-        f = discord.File("images/imageSend.png",filename="imageSend.png")
+        f = self.convertsSVG()
 
         embed=embedsText(f' {name}','') 
         embed.set_image(url="attachment://imageSend.png")
         embed.set_footer(text=f' {hexVal}')
+        await message.delete()
         await ctx.send(file=f, embed=embed)
     
     @commands.command()
     async def complement(self, ctx, arg1):
-         # takes photo url and saves it
+        message = await ctx.send(f"{ctx.message.author.mention} detecting color... might take a few seconds")
         opener = urllib.request.URLopener()
-        opener.addheader('User-Agent', 'whatever')
-        filename, headers = opener.retrieve(arg1, 'images/imageRecieved.jpg')
-
-        # gets dominant color from saved photo
-        color_thief = ColorThief('images/imageRecieved.jpg')
-        dominant_rgb = color_thief.get_color(quality=1)
+        self.convertsIMGRecieved(arg1, opener)
+        dominant_rgb = self.grabsDominantColor()
         
         # gets photo of that color in svg
         jsonurl = urlopen("http://www.thecolorapi.com/scheme?rgb=rgb({0},{1},{2})&mode=complement&count=1".format(dominant_rgb[0],dominant_rgb[1],dominant_rgb[2]))
@@ -65,21 +56,62 @@ class Colors(commands.Cog):
         filename, headers = opener.retrieve(text["image"]["bare"], 'images/tempSVG.svg')
 
         # converts photo into usable format and sends
-        drawing = svg2rlg("images/tempSVG.svg")
-        renderPM.drawToFile(drawing, "images/imageSend.png", fmt="PNG")
-        f = discord.File("images/imageSend.png",filename="imageSend.png")
+        f = self.convertsSVG()
 
+        # embeds message and sends
         embed=embedsText(f' {name}','') 
         embed.set_image(url="attachment://imageSend.png")
-        embed.set_footer(text=f' {hexChosen}')
+        embed.set_footer(text=f' #{hexChosen}')
+        await message.delete()
         await ctx.send(file=f, embed=embed)
 
+    @commands.command()
+    async def scheme(self, ctx, arg1):
+        message = await ctx.send(f"{ctx.message.author.mention} detecting color... might take a few seconds")
+        opener = urllib.request.URLopener()
+        self.convertsIMGRecieved(arg1, opener)
+        dominant_rgb = self.grabsDominantColor()
+        
+        # gets photo of that color in svg
+        jsonurl = urlopen("http://www.thecolorapi.com/scheme?rgb=rgb({0},{1},{2})&count=5".format(dominant_rgb[0],dominant_rgb[1],dominant_rgb[2]))
+        text = json.loads(jsonurl.read())
+        colors = []
+        hexes = []
+        for i in range(0,5):
+            name = text["colors"][i]["name"]["value"]
+            hexVal = text["colors"][i]["name"]["closest_named_hex"]
+            colors.append(name)
+            hexes.append(hexVal)
 
+        filename, headers = opener.retrieve(text["image"]["bare"], 'images/tempSVG.svg')
 
+        # converts photo into usable format and sends
+        f = self.convertsSVG()
 
+        # embeds message and sends
+        embed=embedsText(f' {", ".join(colors)}','') 
+        embed.set_image(url="attachment://imageSend.png")
+        embed.set_footer(text=f' {", ".join(hexes)}')
+        await message.delete()
+        await ctx.send(file=f, embed=embed)
     
-    
-    
+        # takes photo url and saves it
+    def convertsIMGRecieved(self,url, opener):
+        opener.addheader('User-Agent', 'whatever')
+        filename, headers = opener.retrieve(url, 'images/imageRecieved.jpg')
+
+    # gets dominant (most widely used) color from image
+    def grabsDominantColor(self):
+        color_thief = ColorThief('images/imageRecieved.jpg')
+        return color_thief.get_color(quality=1)
+
+    # converts photo into usable format
+    def convertsSVG(self):
+        drawing = svg2rlg("images/tempSVG.svg")
+        renderPM.drawToFile(drawing, "images/imageSend.png", fmt="PNG")
+        return discord.File("images/imageSend.png",filename="imageSend.png")
+
 # Required for the cog to be read by the bot
 def setup(client):
     client.add_cog(Colors(client))
+

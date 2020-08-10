ffbe91bfa2bea0c459a9ad85f28c360782551d88
abajajsingh <aikasbajaj@protonmail.com>
2020-08-09 23:40:14 -0400

    need to fix the db not storing data between uses

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -6,13 +6,20 @@
 import discord
 from discord.ext import commands
 import datetime
+import re
+import sqlite3
+import urllib.request
+from instabot import Bot
 from index import embedsText
+from decouple import config
+from crop import prepare_and_fix_photo
+from urllib.request import urlopen
 
-shareArtChannel = 728836523214110730
-modChannel = 728836900852465716
-
-
+shareArtChannel = int(config('SHARE_ART_CHANNEL'))
+modChannel = int(config('MOD_SOCIAL_ART_GALLERY'))
+botID = int(config('BOT_ID'))
 smRole = 'social media'
+
 
 class SocialMedia(commands.Cog):
     def __init__(self, client):
@@ -23,14 +30,106 @@ class SocialMedia(commands.Cog):
     async def on_message(self,message):
         if message.channel.id == shareArtChannel:
              for role in message.author.roles:
-                if role.name == 'social media' and len(message.attachments) >= 1:
-                    embed = embedsText("New image from #share-your-art",'')
-                    embed.set_image(url=message.attachments[0].url)
-                    embed.set_footer(text=f"{message.author} on {datetime.datetime.now().date()}")
-                    await self.client.get_channel(modChannel).send(embed=embed)
-            
+                if role.name == 'social media' and len(message.attachments) > 0:
+                    for i in range(0, len(message.attachments)):
+                        url = message.attachments[i].url
+                        embed = embedsText("New image from #share-your-art",'')
+                        embed.set_image(url=url)
+                        embed.set_footer(text=f"{message.author} on {datetime.datetime.now().date()}")
+                        bot_msg = await self.client.get_channel(modChannel).send(embed=embed)
+
+                        # stores message information into database ()
+                        conn = sqlite3.connect('example.db')
+                        c = conn.cursor()
+                        val = (bot_msg.id, message.id, url, 0, 0)
+                        c.execute("INSERT INTO shared_art(bot_message_id,original_message_id,image_url,twitter,instagram) VALUES(?,?,?,?,?)", val)
+                        conn.commit()
+                        conn.close()
+
+                        await bot_msg.add_reaction('🐦')
+                        await bot_msg.add_reaction('📷')
+                        await bot_msg.add_reaction('❌')
+    
+    @commands.Cog.listener()
+    async def on_reaction_add(self,reaction,user):
+        if reaction.message.channel.id == modChannel and user.id != botID:
+            conn = sqlite3.connect('example.db')
+            c = conn.cursor()
+            c.execute(f"SELECT * FROM shared_art WHERE bot_message_id = {reaction.message.id}")
+            result = c.fetchone() 
+
+            if result is not None:
+                channel = self.client.get_channel(modChannel)
+                bot_msg = await channel.fetch_message(result[0])
+                url = result[2]
+                if str(reaction.emoji) == "❌":
+                    await bot_msg.delete()
+                elif str(reaction.emoji) == "🐦":
+                    await bot_msg.remove_reaction('🐦',user)
+                    await channel.send("this would work if twitter ACTUALLY ACCEPTED OUR APPLICATION")
+                elif str(reaction.emoji) == "📷":
+                    await bot_msg.remove_reaction('📷',user)
+                    if int(result[4]) == 1:
+                        return await channel.send("this picture has already been posted to instagram")
+
+                    def check(message):
+                        return message.author.id == user.id
+
+                    response = 'n'
+                    description = None 
+                    while response is not 'y':
+                        await channel.send("Type the description you would like to post")
+                        description = await self.client.wait_for('message', check=check, timeout=60.0)
+                        description = description.content
+                        await channel.send(f"Posting \"{description}\"\nIs that ok? ``y/n``")
+                        response = await self.client.wait_for('message', check=check, timeout=60.0)
+                        response = response.content
+
+                    val = (1, result[0])
+                    c.execute("UPDATE shared_art SET instagram = ? WHERE bot_message_id = ?", val)
+                    conn.commit()
+                    conn.close()
+
+                    await self.postInstagram(url, description, channel)    
+                    await channel.send("Posted to instagram :)") 
+                    
 
 
+
+    # Sends to Twitter from message url
+    @commands.command()
+    async def postTwitter(self,ctx,url):
+        try:
+            m = re.findall('\d[0-9]+',url)
+            channelID = int(m[1])
+            msgID = int(m[2])
+            msg  = await self.client.get_channel(channelID).fetch_message(msgID)
+            await ctx.send(msg.attachments[0].url) 
+        except Exception as e:
+            print(e)
+            await ctx.send('Could not post. You suck')
+
+    # Sends to Instagram from message url
+    async def postInstagram(self,url, description, channel):
+        try:
+            opener = urllib.request.URLopener()
+            opener.addheader('User-Agent', 'whatever')
+            photo = 'images/post.jpg'
+            filename, headers = opener.retrieve(url, photo)
+
+            bot = Bot()
+            bot.login(username=config('INSTAGRAM_USERNAME'),
+                        password=config('INSTAGRAM_PASSWORD'))
+
+            prepare_and_fix_photo(photo)
+            bot.upload_photo(photo, 
+                    caption = description)
+        
+        except Exception as e:
+            print(e)
+        
+    
 # Required for the cog to be read by the bot
 def setup(client):
     client.add_cog(SocialMedia(client))
+
---
+++ crop.py
@@ -0,0 +1,70 @@
+# The crop function as used to upload images to instagram.
+# I am not smart enough to create this all credit goes to bansijholt
+# on GitHub (https://github.com/basnijholt/instacron/blob/master/instacron.py#L262)
+
+import instabot
+import numpy as np
+import parse
+import PIL.Image
+
+def prepare_and_fix_photo(photo):
+    with open(photo, "rb") as f:
+        img = PIL.Image.open(f)
+        img =img.convert("RGB")
+        img = strip_exif(img)
+        if not correct_ratio(photo):
+            img = crop_maximize_entropy(img)
+        img.save(photo)
+    return photo
+
+
+def _entropy(data):
+    """Calculate the entropy of an image"""
+    hist = np.array(PIL.Image.fromarray(data).histogram())
+    hist = hist / hist.sum()
+    hist = hist[hist != 0]
+    return -np.sum(hist * np.log2(hist))
+
+
+def crop(x, y, data, w, h):
+    x = int(x)
+    y = int(y)
+    return data[y : y + h, x : x + w]
+
+
+def crop_maximize_entropy(img, min_ratio=4 / 5, max_ratio=90 / 47):
+    from scipy.optimize import minimize_scalar
+
+    w, h = img.size
+    data = np.array(img)
+    ratio = w / h
+    if ratio > max_ratio:  # Too wide
+        w_max = int(max_ratio * h)
+
+        def _crop(x):
+            return crop(x, y=0, data=data, w=w_max, h=h)
+
+        xy_max = w - w_max
+    else:  # Too narrow
+        h_max = int(w / min_ratio)
+
+        def _crop(y):
+            return crop(x=0, y=y, data=data, w=w, h=h_max)
+
+        xy_max = h - h_max
+
+    to_minimize = lambda xy: -_entropy(_crop(xy))  # noqa: E731
+    x = minimize_scalar(to_minimize, bounds=(0, xy_max), method="bounded").x
+    return PIL.Image.fromarray(_crop(x))
+
+
+def strip_exif(img):
+    """Strip EXIF data from the photo to avoid a 500 error."""
+    data = list(img.getdata())
+    image_without_exif = PIL.Image.new(img.mode, img.size)
+    image_without_exif.putdata(data)
+    return image_without_exif
+
+def correct_ratio(photo):
+    from instabot.api.api_photo import compatible_aspect_ratio, get_image_size
+    return compatible_aspect_ratio(get_image_size(photo))

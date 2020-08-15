af03660e422327b15671d055d24c47379d0cbf5a
stringlapse <24516915+stringlapse@users.noreply.github.com>
2020-08-14 20:50:09 -0600

    Added shutdown command
    Causes some "event loop is closed" errors on shutdown but from what I can tell I think it's nothing to worry about

--- index.py
+++ index.py
@@ -6,6 +6,8 @@ from decouple import config
 
 bot = commands.Bot(command_prefix = config('PREFIX'))
 
+admin_role = "devs"
+
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
@@ -13,6 +15,13 @@ async def load(ctx, extension):
 @bot.command()
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
+
+@bot.command()
+@commands.has_role(admin_role)
+async def shutdown(ctx):
+    await ctx.send("Shutting down bot...")
+    print(f"{str(ctx.message.author)} (id {str(ctx.message.author.id)}) requested a shutdown")
+    await bot.close()
 
 def embedsText(title, description):
     if title == '':

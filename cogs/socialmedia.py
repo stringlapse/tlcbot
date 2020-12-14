4bf4e3d647bed701da3c0a23f5e84b93db7d0470
Aikas <54908498+abajajsingh@users.noreply.github.com>
2020-12-14 17:09:34 -0500

    related to #93 but hashtags supported by "&&"

--- cogs/socialmedia.py
+++ cogs/socialmedia.py
@@ -118,8 +118,8 @@ class SocialMedia(commands.Cog):
                         description = None
                         
                         while response != 'y':
-                            msg = await channel.send("Type the description you would like to post. Because discord uses the **@** character to also ping users use the **%%** in place of that. For example, **%%AdamSandler** instead of **@AdamSandler**. Type only ``q`` to quit <@" + str(payload.user_id) + ">")
-                            description = await self.client.wait_for('message', check=check, timeout=60.0)
+                            msg = await channel.send("Type the description you would like to post. Because discord uses the **@** character to also ping users use the **%%** in place of that. For example, **%%AdamSandler** instead of **@AdamSandler**. Use **&&** in place of **#**. Type only ``q`` to quit <@" + str(payload.user_id) + ">")
+                            description = await self.client.wait_for('message', check=check, timeout=120.0)
                             #print("description: ")
                             #print(description.content)
                             channel_re = re.compile(r'<#([0-9]+)>')
@@ -137,6 +137,7 @@ class SocialMedia(commands.Cog):
                                 break
 
                             description = description.replace("%%","@")
+                            description = description.replace("&&","#")
                             if twitter:
                                 while len(description) > 280:
                                     await channel.send(f"<@{str(payload.user_id)}> This tweet ``{description}`` exceeds the 280 character max. Type one with less characters")
@@ -150,6 +151,7 @@ class SocialMedia(commands.Cog):
                                     description = description.content
                             
                             description = description.replace("%%","@")
+                            description = description.replace("&&","#")
                             await channel.send(f"Posting \"{description}\"\nIs that ok? ``y/n``")
                             response = await self.client.wait_for('message', check=check, timeout=60.0)
                             response = response.content
